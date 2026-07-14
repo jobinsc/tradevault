2ed2b628b8de0c0b8b1e8c239a079576811ab3c0
jobinsc <jobinsc@gmail.com>
2026-07-14 16:12:54 +0530

    Fix Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDls6Fe9iC5sEk8KWAULUxFO3I6t_ZyawU",
+  authDomain: "tradevault-994d3.firebaseapp.com",
+  projectId: "tradevault-994d3",
+  storageBucket: "tradevault-994d3.firebasestorage.app",
+  messagingSenderId: "1091497428956",
+  appId: "1:1091497428956:web:afa093f2dbe21b7a5fb119",
+  measurementId: "G-MJ7S998SJ4"
 };
-
 // Admin emails - ONLY these emails can access admin panel
 export const ADMIN_EMAILS = ['jobinsc@gmail.com'];
 
